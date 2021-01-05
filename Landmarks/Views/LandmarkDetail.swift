6c6620668986d17c7b432678bb2be7b13248b0fd
Matt Burke <matt.burke@artisantechgroup.com>
2021-01-05 13:36:03 -0600

    nav list

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Matt Burke on 1/5/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+
+            CircleImage()
+                .offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+                .padding(/*@START_MENU_TOKEN@*/.bottom, -130.0/*@END_MENU_TOKEN@*/)
+
+
+            VStack(alignment: .leading) {
+
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+
+                Text("Descriptive text goes here")
+                    .font(.subheadline)
+
+            }.padding()
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
-        }
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        }.navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .previewDevice(/*@START_MENU_TOKEN@*/"iPhone 11"/*@END_MENU_TOKEN@*/)
     }
 }
