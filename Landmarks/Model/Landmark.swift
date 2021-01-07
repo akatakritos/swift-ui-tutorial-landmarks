724db36fb6cf1e85b35638d31909e7c9124f5ea7
Matt Burke <matt.burke@artisantechgroup.com>
2021-01-07 17:12:50 -0600

    show start

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -10,12 +10,14 @@ import SwiftUI
 import CoreLocation
 
 struct Landmark: Hashable, Codable, Identifiable {
-    var id: Int;
-    var name: String;
-    var park: String;
-    var state: String;
-    var description: String;
-    private var imageName: String;
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
+
+    private var imageName: String
 
     var image: Image {
         Image(imageName)
