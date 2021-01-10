914a6db1d28ddb04578451ef2518962d2e0207e4
Matt Burke <matt.burke@artisantechgroup.com>
2021-01-09 19:12:16 -0600

    start on badge icon

---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,52 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Matt Burke on 1/9/21.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagoParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+
+    static let adjustment = 0.085
+
+    static let segments = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20),
+            curve:   CGPoint(x: 0.00, y: 0.30),
+            control: CGPoint(x: 0.00, y: 0.25)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70),
+            curve:   CGPoint(x: 0.05, y: 0.80),
+            control: CGPoint(x: 0.00, y: 0.75)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80),
+            curve:   CGPoint(x: 1.00, y: 0.70),
+            control: CGPoint(x: 1.00, y: 0.75)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30),
+            curve:   CGPoint(x: 0.95, y: 0.20),
+            control: CGPoint(x: 1.00, y: 0.25)
+        )
+    ]
+}
