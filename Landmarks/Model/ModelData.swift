a05856bfafc71fb0843a66cf0129a239432494ce
Matt Burke <matt.burke@artisantechgroup.com>
2021-01-15 17:07:23 -0600

    nav and group view

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -12,6 +12,14 @@ final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
 
     var hikes: [Hike] = load("hikeData.json")
+
+    var categories: [String: [Landmark]] {
+        Dictionary(grouping: landmarks, by: { $0.category.rawValue })
+    }
+
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
 }
 
 
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    enum Tab {
+        case featured
+        case list
+    }
+
+    @State private var selection: Tab = .featured
+
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+
+            LandmarkList()
+                .tabItem { Label("List", systemImage: "list.bullet") }
+                .tag(Tab.list)
+        }
     }
 }
 
